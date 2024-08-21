a39f07ba5b75a2ee40d832d98dda047825dae94a
Yuchan Lee <lasuillard@gmail.com>
2024-08-21 09:59:49 +0900

    chore: Bad trailing backslash
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -6,7 +6,7 @@ CARGO_BINSTALL_VERSION="1.8.0"
 
 # 1Password
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg \
-  && echo "deb [arch=${ARCH} signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/${ARCH} stable main" | sudo tee /etc/apt/sources.list.d/1password.list \
+  && echo "deb [arch=${ARCH} signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/${ARCH} stable main" | sudo tee /etc/apt/sources.list.d/1password.list
 
 sudo apt-get update && sudo apt-get install -y \
   1password-cli \
